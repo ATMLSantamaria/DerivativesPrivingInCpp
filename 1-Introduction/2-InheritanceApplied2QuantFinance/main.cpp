ab847790d08e2ac895c4f04f339ac48902b1db06
ATMLSantamaria <alejandroteodoromlsh@gmail.com>
2025-02-13 22:35:54 +0100

    main

---
+++ 1-Introduction/2-InheritanceApplied2QuantFinance/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "PayOffPut.h"
+#include "PayOffCall.h"
+
+
+
+int main(){
+  std::cout << "Hello world\n";
+  
+  PayOffCall call = PayOffCall(3);
+  PayOffPut put = PayOffPut(3);
+
+
+
+}
